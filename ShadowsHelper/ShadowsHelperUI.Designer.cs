6d5756863a7315fbb7315c7be87f7afc2f1e9f0b
Atro Karvinen <atro.karvinen@roimaint.com>
2019-12-13 13:21:30 +0200

    Initial implementation of scalable UI

---
+++ ShadowsHelper/ShadowsHelperUI.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ShadowsHelper
+{
+    partial class ShadowsHelperUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OriginalPB = new System.Windows.Forms.PictureBox();
+            this.ResultPB = new System.Windows.Forms.PictureBox();
+            this.OriginalImageLabel = new System.Windows.Forms.Label();
+            this.ResultImageLabel = new System.Windows.Forms.Label();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.SaveImageButton = new System.Windows.Forms.Button();
+            this.ComputeButton = new System.Windows.Forms.Button();
+            this.ShadowLevelsInput = new System.Windows.Forms.TextBox();
+            this.ShadowLevelsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OriginalPB
+            // 
+            this.OriginalPB.Location = new System.Drawing.Point(281, 38);
+            this.OriginalPB.Name = "OriginalPB";
+            this.OriginalPB.Size = new System.Drawing.Size(100, 50);
+            this.OriginalPB.TabIndex = 0;
+            this.OriginalPB.TabStop = false;
+            // 
+            // ResultPB
+            // 
+            this.ResultPB.Location = new System.Drawing.Point(454, 38);
+            this.ResultPB.Name = "ResultPB";
+            this.ResultPB.Size = new System.Drawing.Size(100, 50);
+            this.ResultPB.TabIndex = 1;
+            this.ResultPB.TabStop = false;
+            // 
+            // OriginalImageLabel
+            // 
+            this.OriginalImageLabel.AutoSize = true;
+            this.OriginalImageLabel.Location = new System.Drawing.Point(278, 22);
+            this.OriginalImageLabel.Name = "OriginalImageLabel";
+            this.OriginalImageLabel.Size = new System.Drawing.Size(97, 13);
+            this.OriginalImageLabel.TabIndex = 2;
+            this.OriginalImageLabel.Text = "OriginalImageLabel";
+            // 
+            // ResultImageLabel
+            // 
+            this.ResultImageLabel.AutoSize = true;
+            this.ResultImageLabel.Location = new System.Drawing.Point(451, 22);
+            this.ResultImageLabel.Name = "ResultImageLabel";
+            this.ResultImageLabel.Size = new System.Drawing.Size(92, 13);
+            this.ResultImageLabel.TabIndex = 3;
+            this.ResultImageLabel.Text = "ResultImageLabel";
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(34, 342);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadImageButton.TabIndex = 4;
+            this.LoadImageButton.Text = "LoadImageButton";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveImageButton
+            // 
+            this.SaveImageButton.Location = new System.Drawing.Point(34, 371);
+            this.SaveImageButton.Name = "SaveImageButton";
+            this.SaveImageButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveImageButton.TabIndex = 4;
+            this.SaveImageButton.Text = "SaveImageButton";
+            this.SaveImageButton.UseVisualStyleBackColor = true;
+            // 
+            // ComputeButton
+            // 
+            this.ComputeButton.Location = new System.Drawing.Point(34, 400);
+            this.ComputeButton.Name = "ComputeButton";
+            this.ComputeButton.Size = new System.Drawing.Size(75, 23);
+            this.ComputeButton.TabIndex = 5;
+            this.ComputeButton.Text = "ComputeButton";
+            this.ComputeButton.UseVisualStyleBackColor = true;
+            // 
+            // ShadowLevelsInput
+            // 
+            this.ShadowLevelsInput.Location = new System.Drawing.Point(322, 378);
+            this.ShadowLevelsInput.Name = "ShadowLevelsInput";
+            this.ShadowLevelsInput.Size = new System.Drawing.Size(100, 20);
+            this.ShadowLevelsInput.TabIndex = 6;
+            // 
+            // ShadowLevelsLabel
+            // 
+            this.ShadowLevelsLabel.AutoSize = true;
+            this.ShadowLevelsLabel.Location = new System.Drawing.Point(240, 381);
+            this.ShadowLevelsLabel.Name = "ShadowLevelsLabel";
+            this.ShadowLevelsLabel.Size = new System.Drawing.Size(76, 13);
+            this.ShadowLevelsLabel.TabIndex = 7;
+            this.ShadowLevelsLabel.Text = "Shadow levels";
+            // 
+            // ShadowsHelperUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShadowLevelsLabel);
+            this.Controls.Add(this.ShadowLevelsInput);
+            this.Controls.Add(this.ComputeButton);
+            this.Controls.Add(this.SaveImageButton);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.ResultImageLabel);
+            this.Controls.Add(this.OriginalImageLabel);
+            this.Controls.Add(this.ResultPB);
+            this.Controls.Add(this.OriginalPB);
+            this.Name = "ShadowsHelperUI";
+            this.Text = "Shadows helper";
+            this.Resize += new System.EventHandler(this.ShadowsHelperUI_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox OriginalPB;
+        private System.Windows.Forms.PictureBox ResultPB;
+        private System.Windows.Forms.Label OriginalImageLabel;
+        private System.Windows.Forms.Label ResultImageLabel;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.Button SaveImageButton;
+        private System.Windows.Forms.Button ComputeButton;
+        private System.Windows.Forms.TextBox ShadowLevelsInput;
+        private System.Windows.Forms.Label ShadowLevelsLabel;
+    }
+}
+
---
+++ ShadowsHelper/ShadowsHelperUI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShadowsHelper
+{
+    public partial class ShadowsHelperUI : Form
+    {
+        private int _ScreenWidth;
+        private int _ScreenHeight;
+
+        public ShadowsHelperUI()
+        {
+            InitializeComponent();
+
+            Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            //Rectangle resolution = new Rectangle(0, 0, 1000, 1000);
+            ClientSize = new Size(resolution.Width - 100, resolution.Height - 100);
+        }
+
+        private void InitializeMainScreen()
+        {
+            _ScreenWidth = ClientSize.Width;
+            _ScreenHeight = ClientSize.Height;
+
+            // OriginalPB
+            OriginalPB.Location = Prcnt2PxLoc(2, 2);
+            OriginalPB.Size = Prcnt2PxSize(45, 80);
+            OriginalPB.BorderStyle = BorderStyle.FixedSingle;
+
+            // ResultPB
+            ResultPB.Location = Prcnt2PxLoc(50, 2);
+            ResultPB.Size = Prcnt2PxSize(45, 80);
+            ResultPB.BorderStyle = BorderStyle.FixedSingle;
+
+            // OriginalImageLabel
+            OriginalImageLabel.Location = Prcnt2PxLoc(2, 0);
+            OriginalImageLabel.Size = new Size(97, 13);
+            OriginalImageLabel.Text = "OriginalImageLabel";
+
+            // ResultImageLabel
+            ResultImageLabel.Location = Prcnt2PxLoc(50, 0);
+            ResultImageLabel.Size = new Size(92, 13);
+            ResultImageLabel.Text = "ResultImageLabel";
+
+            // LoadImageButton
+            LoadImageButton.Location = Prcnt2PxLoc(2, 85);
+            LoadImageButton.Size = Prcnt2PxSize(5, 5);
+            LoadImageButton.Text = "Load image";
+
+            // ComputeButton
+            ComputeButton.Location = Prcnt2PxLoc(10, 85);
+            ComputeButton.Size = Prcnt2PxSize(5, 5);
+            ComputeButton.Text = "Compute";
+
+            // SaveImageButton
+            SaveImageButton.Location = Prcnt2PxLoc(18, 85);
+            SaveImageButton.Size = Prcnt2PxSize(5, 5);
+            SaveImageButton.Text = "Save image";
+
+            // ShadowLevelsLabel
+            ShadowLevelsLabel.Location = Prcnt2PxLoc(26, 85);
+            ShadowLevelsLabel.Size = new Size(76, 13);
+            ShadowLevelsLabel.Text = "Shadow levels";
+
+            // ShadowLevelsInput
+            ShadowLevelsInput.Location = Prcnt2PxLoc(31, 85);
+            ShadowLevelsInput.Size = new Size(100, 20);
+            ShadowLevelsInput.Text = "5";
+        }
+
+        private Point Prcnt2PxLoc(double percentWidth, double percentHeight)
+        {
+            int width = (int)Math.Round(_ScreenWidth * percentWidth / 100.0);
+            int height = (int)Math.Round(_ScreenHeight * percentHeight / 100.0);
+            return new Point(width, height);
+        }
+
+        private Size Prcnt2PxSize(double percentWidth, double percentHeight)
+        {
+            Point p = Prcnt2PxLoc(percentWidth, percentHeight);
+            return new Size(p.X, p.Y);
+        }
+
+        private void ShadowsHelperUI_Resize(object sender, EventArgs e)
+        {
+            InitializeMainScreen();
+        }
+    }
+}
+
